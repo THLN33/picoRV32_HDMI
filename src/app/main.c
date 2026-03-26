8ccd2eef97a1d2835ad8cd80945d5a5a2e5058bd
Thierry <thln33@free.fr>
2026-03-26 14:12:04 +0100

    Update main.c and linker options

--- src/app/main.c
+++ src/app/main.c
@@ -76,8 +76,9 @@ int main(void) {
 
 	set_leds(~v);
 
-	//video_text_demo();
+	video_text_demo();
 
+#ifdef _Z
 	uint32_t cpu_freq = system_config_get_cpu_frequency();
 	uart_set_div(cpu_freq / 115200);
 
@@ -86,16 +87,7 @@ int main(void) {
 
 	uint8_t line = 0;
 	while(1) {
-/*
-		for (int i=0;i<40;i++) {
-			display_string("This is a test for Video_Text_96x42 module line = 0x");
-			display_hex(line++, 4);
-			display_string("\r\n");
-			display_string("\r\n");
-			display_string("********************************************************************************************************************************");
-			display_string("\r\n");
-		}
-*/
+
 		display_string("Led = ");
 		display_hex(v, 2);
 		display_string("\r\n");
@@ -110,4 +102,5 @@ int main(void) {
 
 	    delay_ticks(cpu_freq>>2);
 	}
+#endif
 }
